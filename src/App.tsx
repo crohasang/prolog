88f47a0525ff5e2aa37808ab31b734001802ea89
HS <hasang2691@naver.com>
2024-02-25 01:13:15 +0900

    Feat: connect to github

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="font-pretendard text-3xl font-bold underline">
+        it goes Work!!
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    fontFamily: {
+      pretendard: ['Pretendard', 'sans-serif'],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
