baa3f4fd082b93a596dc74c384eed4280b3afe74
HS <hasang2691@naver.com>
2024-02-29 10:33:38 +0900

    Feat: Apply useParams in search page

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,7 +1,18 @@
-import React from 'react';
+import { useParams } from 'react-router-dom';
+import BoardHeader from '../components/organisms/BoardHeader';
 
 const Search = () => {
-  return <div>Search</div>;
+  const { searchWord } = useParams();
+  return (
+    <div className="min-h-screen w-screen bg-white dark:bg-zinc-700 flex flex-col px-8 ">
+      <BoardHeader />
+      <div className="font-pretendard sm:mx-32 mt-20">
+        <div className="text-2xl font-bold dark:text-white mt-4">
+          '{searchWord}' 검색 결과
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Search;
