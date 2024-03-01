5f59b7cd325e291164ba7d4b54408f88f2296b44
HS <hasang2691@naver.com>
2024-03-01 09:50:26 +0900

    Feat: Change background image of intro page

--- src/pages/Intro.tsx
+++ src/pages/Intro.tsx
@@ -1,7 +1,8 @@
 import IntroHeader from '../components/organisms/IntroHeader';
 import IntroTitle from '../components/atoms/Intro/IntroTitle';
 
-import IntroBackgroundVideo from '../assets/videos/introBackGroundVideo3.mp4';
+import IntroBackGroundImage from '../assets/images/introBackgroundImage.jpg';
+
 import GoogleLoginBtn from '../components/atoms/GoogleLoginBtn';
 import EmailLoginBtn from '../components/atoms/EmailLoginBtn';
 import EmailRegisterBtn from '../components/atoms/EmailRegisterBtn';
@@ -9,29 +10,13 @@ import EmailRegisterBtn from '../components/atoms/EmailRegisterBtn';
 const Intro = () => {
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <video
-        autoPlay
-        loop
-        muted
-        playsInline
-        style={{
-          position: 'absolute',
-          width: '100%',
-          left: '50%',
-          top: '50%',
-          height: '100%',
-          objectFit: 'cover',
-          transform: 'translate(-50%, -50%)',
-          zIndex: '-1',
-        }}
+      <div
+        className="h-screen w-full flex flex-col justify-center items-center bg-cover bg-no-repeat bg-center"
+        style={{ backgroundImage: `url(${IntroBackGroundImage})` }}
       >
-        <source src={IntroBackgroundVideo} type="video/mp4" />
-      </video>
-      <IntroHeader />
-      <div className="flex flex-col justify-center items-center gap-y-5">
-        <div className="mb-4">
-          <IntroTitle />
-        </div>
+        <IntroHeader />
+
+        <IntroTitle />
 
         <EmailLoginBtn />
 
