695a2c091ad3e6dd506ab49c5607b0e98a8aeed9
HS <hasang2691@naver.com>
2024-03-04 03:02:05 +0900

    Fix: Change variable name of .env and GoogleLoginBtn because I use Vite

--- src/components/atoms/GoogleLoginBtn.tsx
+++ src/components/atoms/GoogleLoginBtn.tsx
@@ -6,8 +6,8 @@ const GoogleLoginBtn = () => {
 
   const handleLoginClick = () => {
     window.location.href = `https://accounts.google.com/o/oauth2/v2/auth?
-		client_id=${process.env.REACT_APP_GOOGLE_AUTH_CLIENT_ID}
-		&redirect_uri=${process.env.REACT_APP_GOOGLE_AUTH_REDIRECT_URI}
+		client_id=${import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}
+		&redirect_uri=${import.meta.env.VITE_GOOGLE_AUTH_REDIRECT_URI}
 		&response_type=code
 		&scope=email profile`;
   };
