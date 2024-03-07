7d7a4d01855f116584d295820313f0cc8fb4075e
HS <hasang2691@naver.com>
2024-03-07 14:26:45 +0900

    Feat: Create editing in Content page

--- src/pages/Content.tsx
+++ src/pages/Content.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import { useParams } from 'react-router-dom';
 // import { cardData } from '../store/data/card';
 // import { CardResult } from '../store/type/card/card';
@@ -10,8 +10,32 @@ import CommentSection from '../components/organisms/CommentSection';
 import { useQuery } from '@tanstack/react-query';
 import { fetchDetailData } from '../apis/detail/fetchDetailData';
 import Loading from './Loading';
+import BlueBtn from '../components/atoms/BlueBtn';
+import RedBtn from '../components/atoms/RedBtn';
+import WritingEditor from '../components/organisms/WritingEditor';
 
 const Content: React.FC = () => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedTitle, setEditedTitle] = useState('');
+  const [editedBody, setEditedBody] = useState('');
+
+  // 수정 버튼을 눌렀을 때
+  const handleEditClick = () => {
+    if (DetailData !== undefined) {
+      setIsEditing(true);
+      setEditedTitle(DetailData.result.title);
+      setEditedBody(DetailData.result.body);
+    }
+  };
+
+  const handleTitleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setEditedTitle(e.target.value);
+  };
+
+  const handleBodyChange = (e: string) => {
+    setEditedBody(e);
+  };
+
   // fetchDetailData
   const {
     data: DetailData,
@@ -31,15 +55,35 @@ const Content: React.FC = () => {
     <div className="font-pretendard min-h-screen w-screen bg-white dark:bg-zinc-700 flex flex-col px-8 mb-10">
       <BoardHeader />
       <div className=" text-black sm:mx-32 mt-20">
-        <div className="text-2xl font-bold dark:text-white mt-4">
-          {DetailData.result.title}
-        </div>
-        <div className="text-sm text-gray-400 dark:text-white mt-4">
-          {DetailData.result.author} | {DetailData.result.time}
-        </div>
-        <div className="h-auto bg-gray-100 dark:bg-gray-800 dark:text-white rounded-lg mt-10 p-4 ">
-          {DetailData?.result.body}
-        </div>
+        {isEditing ? (
+          <>
+            <input
+              value={editedTitle}
+              onChange={handleTitleChange}
+              className="w-full p-2 border rounded-md h-10 my-4"
+            />
+            <WritingEditor value={editedBody} onChange={handleBodyChange} />
+            <div className="whitespace-nowrap float-right mt-10">
+              <BlueBtn title="제출하기" />
+            </div>
+          </>
+        ) : (
+          <>
+            <div className="text-2xl font-bold dark:text-white mt-4">
+              {DetailData.result.title}
+            </div>
+            <div className="text-sm text-gray-400 dark:text-white mt-4">
+              {DetailData.result.author} | {DetailData.result.time}
+            </div>
+            <div className="h-auto bg-gray-100 dark:bg-gray-800 dark:text-white rounded-lg mt-10 p-4 ">
+              {DetailData?.result.body}
+            </div>
+            <div className="flex gap-x-2 whitespace-nowrap float-right mt-10">
+              <BlueBtn title="수정" onClick={handleEditClick} />
+              <RedBtn title="삭제" />
+            </div>
+          </>
+        )}
       </div>
       <CommentSection CommentResult={DetailData.result.comments} />
       <CommentWrite />
