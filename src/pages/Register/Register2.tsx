df93206dca0a661cfcea0c18affa1c6c53c3414d
HS <hasang2691@naver.com>
2024-03-08 00:35:11 +0900

    Feat: add nicknameCheckMutation

--- src/pages/Register/Register2.tsx
+++ src/pages/Register/Register2.tsx
@@ -4,21 +4,55 @@ import IntroBackGroundImage from '../../assets/images/introBackgroundImage.jpg';
 import { useNavigate } from 'react-router-dom';
 import { useFadeIn } from '../../hooks/useFadeIn';
 import BlueBtn from '../../components/atoms/BlueBtn';
+import { useMutation } from '@tanstack/react-query';
+import instance from '../../apis/instance';
 
 const Register2 = () => {
   const navigate = useNavigate();
   const opacity = useFadeIn(0, 1, 500);
 
-  // const [email, setEmail] = useState<string>('');
+  // 닉네임
   const [nickname, setNickname] = useState<string>('');
+
+  // 회원가입 가능 여부
+  const [isRegisterDisable, setIsRegisterDisable] = useState<boolean>(true);
+
+  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) =>
+    setNickname(e.target.value);
+
+  // 닉네임 중복 검사
+  const nicknameCheckMutation = useMutation({
+    mutationFn: (nickName: string) => {
+      return instance.post('', nickName);
+    },
+    onSuccess: (res) => {
+      // 닉네임이 중복인지 여부 (백엔드와 소통할 것)
+      const isDuplicated = res.data.isDuplicated;
+
+      if (isDuplicated === true) {
+        alert('닉네임이 중복입니다!');
+
+        // 입력한 닉네임 초기화
+        setNickname('');
+      } else {
+        // 가입 버튼 Disable 해제
+        setIsRegisterDisable(false);
+      }
+    },
+  });
+
+  // 중복 검사 클릭했을 때
+  const handleNicknameCheck = () => {
+    nicknameCheckMutation.mutate(nickname);
+  };
+
+  // const [email, setEmail] = useState<string>('');
+
   // const [password, setPassword] = useState<string>('');
   // const [passwordCheck, setPasswordCheck] = useState<string>('');
 
   // const handleEmailChange = (e: ChangeEvent<HTMLInputElement>) =>
   //   setEmail(e.target.value);
-
-  const handleNicknameChange = (e: ChangeEvent<HTMLInputElement>) =>
-    setNickname(e.target.value);
 
   // const handlePasswordChange = (e: ChangeEvent<HTMLInputElement>) =>
   //   setPassword(e.target.value);
@@ -58,7 +92,7 @@ const Register2 = () => {
               className="border-2 border-gray-200 p-2 w-64 text-black"
               placeholder="10자 이하로 입력해주세요"
             />
-            <BlueBtn title="중복 검사" />
+            <BlueBtn title="중복 검사" onClick={handleNicknameCheck} />
           </div>
 
           {/* <div className="text-xl font-bold mt-2 text-black ">비밀번호</div>
@@ -86,7 +120,7 @@ const Register2 = () => {
           <div className="float-right mt-10">
             <BlueBtn
               title="가입하기"
-              disabled={true}
+              disabled={isRegisterDisable}
               onClick={() => navigate('/board')}
             />
           </div>
