19709f357a80356c81bd98cc2f6a016ec7c1f4dc
HS <hasang2691@naver.com>
2024-03-07 13:51:22 +0900

    Feat: Update comment-section when there is no comment

--- src/components/organisms/CommentSection.tsx
+++ src/components/organisms/CommentSection.tsx
@@ -9,9 +9,9 @@ interface Props {
 const CommentSection: React.FC<Props> = ({ CommentResult }) => {
   return (
     <div className="font-pretendard mt-8 sm:mx-32 flex flex-col text-black dark:text-white gap-y-2">
-      {CommentResult !== undefined &&
+      {CommentResult && CommentResult.length > 0 ? (
         CommentResult.map((value) => (
-          <div className="w-full p-2 border rounded-md" key={value.id}>
+          <div className="w-full p-2 border-b rounded-md" key={value.id}>
             <div className="text-sm font-semibold ">{value.author}</div>
             <div className="text-sm font-light mt-2">{value.text}</div>
             <div className="flex justify-between">
@@ -22,7 +22,12 @@ const CommentSection: React.FC<Props> = ({ CommentResult }) => {
               </div>
             </div>
           </div>
-        ))}
+        ))
+      ) : (
+        <div className="flex justify-center items-center h-full">
+          <div>아직 댓글이 없습니다.</div>
+        </div>
+      )}
     </div>
   );
 };
